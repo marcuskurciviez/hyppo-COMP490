b608966d51043d473a145e8c242f1d93870a680c
jriggs935 <62345927+jriggs935@users.noreply.github.com>
2023-05-02 17:40:51 -0400

    Add files via upload
    Added additonal testing involving KFDA and two sample. Nothing should be needed to run it

--- hyppo/ksample/tests/test_ksamp.py
+++ hyppo/ksample/tests/test_ksamp.py
@@ -1,9 +1,13 @@
+from functools import reduce
+from operator import concat
+
 import numpy as np
 import pytest
 from numpy.testing import assert_almost_equal, assert_raises
 
 from ...tools import power, rot_ksamp
 from .. import KSample
+from kfda import Kfda
 
 
 class TestKSample:
@@ -18,6 +22,22 @@ class TestKSample:
 
         assert_almost_equal(stat, obs_stat, decimal=1)
         assert_almost_equal(pvalue, obs_pvalue, decimal=1)
+
+    def test_twosamp_kdf(self):
+        np.random.seed(123456789)
+        NewArray = [5],[5]
+
+        x, y = rot_ksamp("linear", 10, 5, k=2)
+        stat, pvalue = KSample("CCA").test(x, y)
+        for z in range(5,5):
+         for a in range(5,5):
+
+            NewArray[z]=stat
+            NewArray[a]=pvalue
+        cls = Kfda(n_components=2, kernel='linear')
+        cls.fit(NewArray,[2,1])
+        cls.predict(NewArray)
+        assert cls.predict(NewArray).any()
 
     def test_rf(self):
         np.random.seed(123456789)
