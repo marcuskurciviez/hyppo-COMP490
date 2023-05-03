72a46fe2b0d179f8f1d169767babc5cdc8d1eb8f
marcuskurciviez <mkurciviez@student.bridgew.edu>
2023-05-03 11:02:47 -0400

    Cleaned up test_FIT function

--- hyppo/conditional/tests/test_FIT.py
+++ hyppo/conditional/tests/test_FIT.py
@@ -1,28 +1,19 @@
-from operator import concat
-from random import random
-from hyppo.conditional.FIT import cross_validate
-from numpy import floor
-from numpy import np
-from scipy.stats import ttest_1samp
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import GridSearchCV
-import pytest
-import numpy as np
-from hyppo.conditional.FIT import interleave, cv_besttree
+import numpy
+from hyppo.conditional.FIT import interleave, cv_besttree, fit_test
 
 # Test the interleave function
 def test_interleave():
-    x = np.array([[1, 2], [3, 4]])
-    z = np.array([[5, 6], [7, 8]])
+    x = numpy.array([[1, 2], [3, 4]])
+    z = numpy.array([[5, 6], [7, 8]])
     result = interleave(x, z)
     assert result.shape == (2, 4), "Shape of interleaved array is incorrect."
-    assert np.all(np.sort(result, axis=1) == np.array([[1, 2, 5, 6], [3, 4, 7, 8]])), "Interleaving is incorrect."
+    assert numpy.all(numpy.sort(result, axis=1) == numpy.array([[1, 2, 5, 6], [3, 4, 7, 8]])), "Interleaving is incorrect."
 
 # Test the cv_besttree function
 def test_cv_besttree():
-    x = np.random.rand(50, 2)
-    y = np.random.rand(50, 1)
-    z = np.random.rand(50, 2)
+    x = numpy.random.rand(50, 2)
+    y = numpy.random.rand(50, 1)
+    z = numpy.random.rand(50, 2)
     cv_grid = [2, 8]
     logdim = False
     verbose = False
@@ -30,14 +21,3 @@ def test_cv_besttree():
     clf = cv_besttree(x, y, z, cv_grid, logdim, verbose, prop_test)
     assert clf, "cv_besttree should return a DecisionTreeRegressor."
 
-def test_FIT_TEST(x , y , z , n_perm =8 , frac_test=.1):
-    x = np.random.rand(50, 2)
-    y = np.random.rand(50, 1)
-    z = np.random.rand(50, 2)
-    best_tree_x=cross_validate(x,y,z)
-    best_tree_x.fit(np.concatenate((x,z), axis=1), y)
-    mses_x = list()
-    mses_x.append(
-        mses_x(best_tree_x.predict(np.concatenate((x, z), axis=1)), y))
-    mses_x.append(mses_x(best_tree_x.predict(z), y))
-    assert best_tree_x
--- hyppo/ksample/base.py
+++ hyppo/ksample/base.py
@@ -1,15 +1,6 @@
 from abc import ABC, abstractmethod
 from typing import NamedTuple
 
-import numpy as np
-from joblib import Parallel, delayed
-from scipy.stats import percentileofscore
-from sklearn.metrics import pairwise_distances, pairwise_kernels
-
-from examples.permutation_tree import X, Y
-
-from tutorials.ksample import y, x
-
 
 class KSampleTestOutput(NamedTuple):
     stat: float
@@ -56,66 +47,17 @@ class KSampleTest(ABC):
     **kwargs
         Arbitrary keyword arguments for ``compute_distkern``.
     """
-    def permute(self, X, Y, n_perm, block_size, compute_distance=None, **kwargs):
-        n = X.shape[0]
-        m = Y.shape[0]
-        xy = np.vstack([X, Y])
-        indices = np.arange(n + m)
-        shuffle_indices = np.random.permutation(indices)
-        shuffle_indices_X = shuffle_indices[:n]
-        shuffle_indices_Y = shuffle_indices[n:]
 
-        stat_perm = []
-        for i in range(n_perm):
-            block_indices_X = np.split(shuffle_indices_X, range(block_size, n, block_size))
-            block_indices_Y = np.split(shuffle_indices_Y, range(block_size, m, block_size))
-
-            # within-block permutation of X and Y
-            for j in range(len(block_indices_X)):
-                block_indices_X[j] = np.random.permutation(block_indices_X[j])
-            for j in range(len(block_indices_Y)):
-                block_indices_Y[j] = np.random.permutation(block_indices_Y[j])
-
-            # reconstruct the permuted matrix
-            permuted_indices = np.hstack(block_indices_X + block_indices_Y)
-            permuted_xy = xy[permuted_indices]
-
-            # compute test statistic of the permuted matrix
-            stat_perm.append(self.statistic(*np.split(permuted_xy, [n])))
-
-        # calculate p-value
-        self.pvalue = (percentileofscore(stat_perm, self.stat) / 100.0)
     def __init__(self, compute_distance=None, bias=False, **kwargs):
+        # set statistic and p-value
         self.stat = None
         self.pvalue = None
-        self.compute_distance = compute_distance
         self.bias = bias
+        self.compute_distance = compute_distance
         self.kwargs = kwargs
 
         super().__init__()
 
-    def block_permutation(self, X, block_size):
-        n = X.shape[0]
-        n_blocks = n // block_size
-        permuted_X = np.zeros_like(X)
-
-        for i in range(n_blocks):
-            block_start = i * block_size
-            block_end = (i + 1) * block_size
-            block = X[block_start:block_end]
-            permuted_X[block_start:block_end] = np.random.permutation(block)
-
-        return permuted_X
-
-    def compute_distance(self, X, Y=None):
-        if callable(self.compute_distance):
-            return self.compute_distance(X, Y, **self.kwargs)
-        elif self.compute_distance in pairwise_distances.VALID_METRICS:
-            return pairwise_distances(X, Y, metric=self.compute_distance, **self.kwargs)
-        elif self.compute_distance in pairwise_kernels.VALID_KERNELS:
-            return pairwise_kernels(X, Y, metric=self.compute_distance, **self.kwargs)
-        else:
-            raise ValueError("Invalid distance metric specified.")
     @abstractmethod
     def statistic(self, *args):
         r"""
@@ -134,8 +76,9 @@ class KSampleTest(ABC):
         stat : float
             The computed *k*-Sample statistic.
         """
+
     @abstractmethod
-    def test(self, *args, reps=1000, workers=1, random_state=None, block_size=None):
+    def test(self, *args, reps=1000, workers=1, random_state=None):
         r"""
         Calculates the *k*-sample test statistic and p-value.
 
@@ -160,23 +103,3 @@ class KSampleTest(ABC):
         pvalue : float
             The computed *k*-sample p-value.
         """
-        if block_size is not None:
-            # perform block permutation
-            permute_args = (X, Y, reps, block_size, self.compute_distance) + self.kwargs.items()
-            stat_perm = Parallel(n_jobs=workers, verbose=0)(
-                delayed(self._permute)(*permute_args, random_state=random_state + i)
-                for i in range(reps))
-            self.pvalue = np.mean(stat_perm >= self.stat)
-        else:
-            # perform regular permutation
-            perms = np.array([
-                np.random.permutation(np.vstack([x, y]))
-                for _ in range(reps)])
-            pstat = np.array([
-                self.statistic(*np.split(perm, [x.shape[0]]))
-                for perm in perms])
-            self.stat = self.statistic(*args)
-            self.pvalue = np.mean(pstat >= self.stat)
-
-        return KSampleTestOutput(stat=self.stat, pvalue=self.pvalue)
-
