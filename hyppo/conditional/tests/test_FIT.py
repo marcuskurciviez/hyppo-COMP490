b6406db9c0c9126618fec868305f92ae72863d03
marcuskurciviez <mkurciviez@student.bridgew.edu>
2023-05-03 00:27:07 -0400

    Added test functions to test_FIT.py

--- hyppo/conditional/tests/test_FIT.py
+++ hyppo/conditional/tests/test_FIT.py
@@ -6,32 +6,26 @@ from numpy import np
 from scipy.stats import ttest_1samp
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import GridSearchCV
+import pytest
+import numpy as np
+from hyppo.conditional.FIT import interleave, cv_besttree
 
+# Test the interleave function
+def test_interleave():
+    x = np.array([[1, 2], [3, 4]])
+    z = np.array([[5, 6], [7, 8]])
+    result = interleave(x, z)
+    assert result.shape == (2, 4), "Shape of interleaved array is incorrect."
+    assert np.all(np.sort(result, axis=1) == np.array([[1, 2, 5, 6], [3, 4, 7, 8]])), "Interleaving is incorrect."
 
-
-def cross_validate(covarite,regressand):
-
-
-def fit_test (x , y , z , n_perm =8 , frac_test=.1 ):
-    n_samples=x.shape[0]
-    n_test=floor(frac_test*n_samples)
-    best_tree_x=cross_validate(concat(x,z),y)
-    best_tree_nox=cross_validate(z,y)
-    mses_x= list()
-    mses_nox=list()
-    for perm_id in range (n_perm):
-        perm_ids=random.permuations(n_samples)
-        x_test,x_train= x[perm_ids][:n_test],x[perm_id][n_test:]
-        y_test, y_train = y[perm_ids][:n_test], y[perm_id][n_test:]
-        z_test, z_train = z[perm_ids][:n_test], z[perm_id][n_test:]
-        best_tree_x.train(concat(x_train,z_train),y_train)
-        mses_x.append(
-            mse(best_tree_x.predict(concat(x_test,z_test)),y_test))
-        best_tree_nox.train(z_train,y_train)
-        mses_nox.append((mse(best_tree_nox.predict(z_test))))
-
-        t,pval=ttest_1samp(mses_nox-mses_x)
-        if t<0:
-            return 1- pval/2.
-        else:
-            return pval/2.
+# Test the cv_besttree function
+def test_cv_besttree():
+    x = np.random.rand(50, 2)
+    y = np.random.rand(50, 1)
+    z = np.random.rand(50, 2)
+    cv_grid = [2, 8]
+    logdim = False
+    verbose = False
+    prop_test = 0.1
+    clf = cv_besttree(x, y, z, cv_grid, logdim, verbose, prop_test)
+    assert clf, "cv_besttree should return a DecisionTreeRegressor."
